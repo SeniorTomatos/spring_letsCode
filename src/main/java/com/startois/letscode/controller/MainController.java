8a20659e4409691983ab125fa8b566ae84f95284
SeniorTomatos <dehuckoo5@gmail.com>
2022-07-20 12:05:29 +0300

    добавил проверку на наличие файла в обзоре

--- src/main/java/com/startois/letscode/controller/MainController.java
+++ src/main/java/com/startois/letscode/controller/MainController.java
@@ -58,7 +58,7 @@ public class MainController {
             @RequestParam String tag, Map<String, Object> model,
             @RequestParam("file") MultipartFile file) throws IOException {
         Message message = new Message(text, tag, user);
-        if (file != null) {
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
             File uploadDir = new File(uploadPath);
             if (uploadDir.exists()) {
                 uploadDir.mkdir();
